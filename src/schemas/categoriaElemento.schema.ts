00d57d6097b3ab0479c050901f236eb4f8eb6ac5
123Brandongomez <brandogomez403@gmail.com>
2025-04-30 10:54:39 -0500

    realize el manejo de zod en schemas y tanstackquery en lo que es de api en actualizar,crear,mostrar y eliminar en sitios,tipoMateriales,categoriaElementos y materiales

--- src/schemas/categoriaElemento.schema.ts
+++ src/schemas/categoriaElemento.schema.ts
@@ -3,9 +3,13 @@ import { z } from 'zod';
 
 export const categoriaElementoSchema = z.object({
   id_categoria_elemento: z.number({ required_error: 'El id es requerido' }),
+
   codigo_unpsc: z.string().min(1, 'El código UNPSC es requerido'),
+
   nombre_categoria: z.string().min(3, 'El nombre debe tener al menos 3 caracteres').max(50, 'El nombre no puede tener más de 50 caracteres'),
+
   fecha_creacion: z.string().min(1, 'La fecha de creación es requerida'),
+  
   fecha_modificacion: z.string().min(1, 'La fecha de modificación es requerida'),
 });
 
