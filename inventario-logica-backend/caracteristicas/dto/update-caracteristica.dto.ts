0a7ca441ff03651b12d31798ead3757303be5d23
VictorRuiz1061 <vicr357477@gmail.com>
2025-07-03 11:17:42 -0500

    Elimino una carpeta que se subio por accidente

--- inventario-logica-backend/caracteristicas/dto/update-caracteristica.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCaracteristicaDto } from './create-caracteristica.dto';
-
-export class UpdateCaracteristicaDto extends PartialType(CreateCaracteristicaDto) {}
