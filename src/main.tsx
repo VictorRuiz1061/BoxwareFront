3918d332d2d50a801bbb125f3a649b974f89024e
123Brandongomez <brandogomez403@gmail.com>
2025-08-22 19:54:48 -0500

    fix: actualizo .gitignore y main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import App from "@/App.tsx";
 import "@/styles/globals.css";
 const queryClient = new QueryClient();
 
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
